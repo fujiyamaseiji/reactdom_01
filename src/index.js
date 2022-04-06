2ff3faeb67725f858a5aa4c8c117fbbb464eef8d
seiji <fujiyamaseiji@gmail.com>
2022-04-06 10:46:48 +0900

    コンポーネント化

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-const App = () => {
-  return (
-    <>
-      <h1>React 頑張ってる途中</h1>
-    </>
-  );
-};
+import App from "./App";
 
 ReactDom.render(<App />, document.getElementById("root"));
